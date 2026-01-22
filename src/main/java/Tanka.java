4ac5a4881f17fb25f51180d1bc85824d0948a8c4
hari <harikrish.nandakumar@gmail.com>
2026-01-22 22:04:38 +0800

    Level-1

--- src/main/java/Tanka.java
+++ src/main/java/Tanka.java
@@ -10,17 +10,32 @@ public class Tanka {
                 "   ██║   ██║  ██║██║ ╚████║██║  ██╗██║  ██║\n" +
                 "   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝\n" +
                 "\n" +
-                "     J I H A R I\n" +
+                "     J A H A R I\n" +
                 "\n" +
                 "but i would never order a full pizza 🍕 for myself";
                
-        System.out.println("Hello! I'm \n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Tanka Jihari");
-        System.out.println("What can i do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
+            System.out.println("Hello! I'm \n" + logo);
+            System.out.println("____________________________________________________________");
+            System.out.println(" Hello! I'm Tanka Jihari");
+            System.out.println(" What can I do for you?");
+            System.out.println("____________________________________________________________");
+        
+            Scanner scanner = new Scanner(System.in);
+            String userInput = ""; // Initialize userInput with an empty string
+        
+            while (true) {
+                userInput = scanner.nextLine(); // Read user input from the console
+                if (userInput.equals("bye")) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Bye. Hope to see you again soon!");
+                    System.out.println("____________________________________________________________");
+                        break;
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" " + userInput); // Echo the user's input with proper spacing
+                    System.out.println("____________________________________________________________");
+                }
+            }
+            scanner.close();
     }
 }
